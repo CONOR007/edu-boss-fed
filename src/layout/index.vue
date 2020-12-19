aa890f65d0e32aec33479c0fc63bd773bf82cfa6
xkwg <xkwg@163.com>
2020-12-19 14:18:47 +0800

    登录

---
+++ src/layout/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="layout">
+        <el-container>
+            <el-aside width="200px">
+                <app-aside></app-aside>
+            </el-aside>
+            <el-container>
+                <el-header>
+                    <app-header></app-header>
+                </el-header>
+                <el-main>
+                    <router-view />
+                </el-main>
+            </el-container>
+        </el-container>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import AppAside from './components/app-aside.vue'
+import AppHeader from './components/app-header.vue'
+export default Vue.extend({
+  components: {
+    AppAside,
+    AppHeader,
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.el-container{
+    min-height: 100vh;
+    min-width: 980px;
+  .el-header {
+    background-color: #fff;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+  }
+}
+
+</style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,76 @@
 <template>
-  <div class="login">用户登录</div>
+  <div class="login">
+    <el-form :label-position="'top'"
+      ref="form"
+      :model="form"
+      class="loginform"
+      label-width="80px">
+      <el-form-item label="手机号">
+        <el-input v-model="form.phone"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button  class="loginbtn" type="primary" :loading="loading" @click="onSubmit">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-
+import qs from 'qs'
+import request from '@/utils/request'
 export default Vue.extend({
-  name: 'LoginIndex',
+  name: 'Login',
+  data () {
+    return {
+      loading: false,
+      form: {
+        phone: '',
+        password: '',
+      },
+    }
+  },
+  methods: {
+    async onSubmit () {
+      // 1.表单验证
+      // 2.提交表单
+      // 3.处理请求结果 成功=>跳转 失败=>提示
+      this.loading = true
+      const { data } = await request({
+        method: 'POST',
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        data: qs.stringify(this.form),
+        url: '/front/user/login',
+      })
+      console.log(data)
+      this.loading = false
+      if (data.state !== 1) {
+        return this.$message.error(data.message)
+      }
+      this.$router.push({ name: 'home' })
+      this.$message.success(data.message)
+    },
+  },
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.login{
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .loginform{
+    background: #fff;
+    padding: 20px;
+    border-radius: 5px;
+    width: 300px;
+  }
+  .loginbtn{
+    width: 100%;
+  }
+}
+</style>
